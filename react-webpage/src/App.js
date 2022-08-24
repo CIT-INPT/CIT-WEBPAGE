fa4c842891caf4738b9be02349b9ae75ac9ce0fe
ossamalouati <92301144+OssamaLouati@users.noreply.github.com>
2022-08-24 16:01:24 +0100

    update dependencies to es6

--- react-webpage/src/App.js
+++ react-webpage/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/layout/footer/index";
 import Navbar from "./components/layout/navbar/index";
 import Home from "./components/home/Home";
@@ -13,14 +13,14 @@ function App() {
   return (
     <Router>
       <Navbar></Navbar>
-      <Switch>
+      <Routes>
         {/* Les routes + les composants hna à définir */}
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/partners" component={Partners} />
         <Route exact path="/events" component={Events} />
         <Route exact path="/contact" component={Contact} />
-      </Switch>
+      </Routes>
       {/* Le footer à changer */}
       <Footer></Footer>
     </Router>
